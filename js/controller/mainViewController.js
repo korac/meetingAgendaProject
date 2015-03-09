7694694f7e0304c8536b25f792b5a366a309e327
JonaSim <josimsch@hotmail.de>
2015-03-09 10:56:43 +0100

    changed day list to button day list

--- js/controller/mainViewController.js
+++ js/controller/mainViewController.js
@@ -8,11 +8,12 @@ var MainViewController = function (view, model) {
 
 
     this._view.addDayBtn.on("click", function(){
-
         var day = new Day(8,0);
         _this._model.addDay(day);
         console.log(_this._model.days);
-    });
+        //$(<li>).text(something.val(maskinput));
+        prompt("Click Ok");
+    	});
 
 
 }
