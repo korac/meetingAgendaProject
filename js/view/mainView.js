8986c3b4d14637d8286a8e5ee2cb290d603016e3
mushos32 <mushos32@yahoo.gr>
2015-03-09 05:56:13 +0100

    no message

--- js/view/mainView.js
+++ js/view/mainView.js
@@ -6,12 +6,28 @@ var MainView = function (container, model) {
     this._container = container;
     this._model = model;
 
+ this.rightbox = container.find("#rightbox");
+      this.addDayBtn = this._container.find("#addDayBtn");
 
-    this.addDayBtn = this._container.find("#addDayBtn");
 
     this.update = function () {
        
         
-    }
+        
+       //this.rightbox.empty();
+//this is adding the box but it is not visible for some reason,
+        for (var i = 0; i < model.days.length; i++)
+        {
 
+            var dayDiv = $("<div>");
+            dayDiv.addClass('row');
+
+            var dayView = new DayView(dayDiv, model, model.days[i]);
+            var dayController = new DayController(dayView, model, model.days[i]);
+
+            this.rightbox.append(dayDiv);
+        }
+    }
+	 this.update();
+	
 }
