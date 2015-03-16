4245ad6f4348cfb0237c449a94255ee6a10a5d4a
JonaSim <josimsch@hotmail.de>
2015-03-16 12:55:02 +0100

    start validation for modal form

--- js/controller/ScheduleController.js
+++ js/controller/ScheduleController.js
@@ -18,16 +18,65 @@ var ScheduleController = function (view, model) {
 // lets the user put in a date for the new day.
 	$('#addDayBtn').on('click', function() {
 		console.log("Clicked button to add a day");
+        $("#newDayName").val('');
+        $("#newDayDate").val('');
 		$("#newDayModal").modal("show");
 	});
 
-// Here, the code populates the chedule lit on the right side with a new
-// button, showing the text "Date" and "Name". The button is to be clicked
-// later on to toggle the view of this particular day.
-	$('#saveNewDate').on("click", function(event){
-    $("#newDayModal").modal("hide");
-    $('<button type="button" class="btn btn-lg btn-default" id="dayButton">'+ $("#newDayName").val() +'<br>'+ $("#newDayDate").val() +'</button>').attr("id", "day" + day++).appendTo('.btn-group-vertical');event.preventDefault();
 
+
+
+//Checking if field(s) are filled with values
+    function validateName() {
+        if ($('#newDayName').val() == "") {
+              $('#newDayName').addClass("error"); // adding css error class to the control
+              $('#newDayNameInfo').text("This field cannot be empty!");//filling up error message
+              $('#newDayNameInfo').addClass("error");//add error class to info span
+              return false;
+        } else {
+              $('#newDayName').removeClass("error");
+              $('#newDayNameInfo').text("*");
+              $('#newDayNameInfo').removeClass("error");
+        }
+    };
+
+    function validateDate() {
+        if ($('#newDayDate').val() == "") {
+              $('#newDayDate').addClass("error"); // adding css error class to the control
+              $('#newDayDateInfo').text("This field cannot be empty!");//filling up error message
+              $('#newDayDateInfo').addClass("error");//add error class to info span
+              return false;
+        } else {
+              $('#newDayDate').removeClass("error");
+              $('#newDayDateInfo').text("*");
+              $('#newDayDateInfo').removeClass("error");
+        }
+    };
+
+
+
+// Here, the code populates the chedule on the right side with a new
+// button, showing the text "Date" and "Name". The button is to be clicked
+// later on to toggle the view of this particular day. If closed, all is dismissed.
+	$('#saveNewDate').on("click", function(event){
+        if (validateName()) {
+          control.log("button clicked");
+            $("#newDayModal").modal("hide");
+            $('<button type="button" class="btn btn-lg btn-default" id="dayButton">'+ $("#newDayName").val() +'<br>'+ $("#newDayDate").val() +'</button>').attr("id", "day" + day++).appendTo('.btn-group-vertical');
+            event.preventDefault();
+            return(true);
+        }else {
+            $("#newDayModal").modal("show");
+            return(false);
+        };
 
     });
+
+    $('#closeNewDate').on('click', function() {
+            $('#newDayName').removeClass("error");
+            $('#newDayNameInfo').text("*");
+            $('#newDayNameInfo').removeClass("error");
+            control.log(newDayDate.val());
+    });
+
 }
