d6e3204dc0073fa24322ce268c75c0a090a2dc98
JonaSim <josimsch@hotmail.de>
2015-03-16 15:33:32 +0100

    new meeting modal has input validation now and populates the list on the right

--- js/controller/ScheduleController.js
+++ js/controller/ScheduleController.js
@@ -15,68 +15,60 @@ var ScheduleController = function (view, model) {
 
 
 // By clicking the "+ Add Day"-Button, a modal window is opened, which
-// lets the user put in a date for the new day.
+// lets the user put in a date and name for the new day.
 	$('#addDayBtn').on('click', function() {
-		console.log("Clicked button to add a day");
-        $("#newDayName").val('');
-        $("#newDayDate").val('');
-		$("#newDayModal").modal("show");
+      $("#newDayName").val('');
+      $("#newDayDate").val('');
+		  $("#newDayModal").modal("show");
 	});
-
-
-
-
-//Checking if field(s) are filled with values
-    function validateName() {
-        if ($('#newDayName').val() == "") {
-              $('#newDayName').addClass("error"); // adding css error class to the control
-              $('#newDayNameInfo').text("This field cannot be empty!");//filling up error message
-              $('#newDayNameInfo').addClass("error");//add error class to info span
-              return false;
-        } else {
-              $('#newDayName').removeClass("error");
-              $('#newDayNameInfo').text("*");
-              $('#newDayNameInfo').removeClass("error");
-        }
-    };
-
-    function validateDate() {
-        if ($('#newDayDate').val() == "") {
-              $('#newDayDate').addClass("error"); // adding css error class to the control
-              $('#newDayDateInfo').text("This field cannot be empty!");//filling up error message
-              $('#newDayDateInfo').addClass("error");//add error class to info span
-              return false;
-        } else {
-              $('#newDayDate').removeClass("error");
-              $('#newDayDateInfo').text("*");
-              $('#newDayDateInfo').removeClass("error");
-        }
-    };
-
 
 
 // Here, the code populates the chedule on the right side with a new
 // button, showing the text "Date" and "Name". The button is to be clicked
 // later on to toggle the view of this particular day. If closed, all is dismissed.
 	$('#saveNewDate').on("click", function(event){
-        if (validateName()) {
-          control.log("button clicked");
+          console.log($('#newDayDate').val());
+          console.log("button clicked");
+          //removing any error messages/classes
+            $('#newDayDate').removeClass("error");
+            $('#newDayDateInfo').text("*");
+            $('#newDayDateInfo').removeClass("error");
+
+            $('#newDayName').removeClass("error");
+            $('#newDayNameInfo').text("*");
+            $('#newDayNameInfo').removeClass("error");
+      //If everything is alright, add the day button to the list
+        if ($('#newDayName').val() != "" && $('#newDayDate').val() != "") {
             $("#newDayModal").modal("hide");
             $('<button type="button" class="btn btn-lg btn-default" id="dayButton">'+ $("#newDayName").val() +'<br>'+ $("#newDayDate").val() +'</button>').attr("id", "day" + day++).appendTo('.btn-group-vertical');
             event.preventDefault();
-            return(true);
-        }else {
-            $("#newDayModal").modal("show");
-            return(false);
+            return true ;
+      //if something's missing, set an error message
+          }else{
+
+              if ($('#newDayDate').val() === "") {
+                     $('#newDayDate').addClass("error"); // adding css error class to the control
+                     $('#newDayDateInfo').text("Please choose a date.");//filling up error message
+                     $('#newDayDateInfo').addClass("error");//add error class to info span
+               }
+
+              if ($('#newDayName').val() === "") {
+                     $('#newDayName').addClass("error"); // adding css error class to the control
+                     $('#newDayNameInfo').text("Please enter a name.");//filling up error message
+                     $('#newDayNameInfo').addClass("error");//add error class to info span
+               }
+            return false;
         };
-
     });
-
+//If the modal is closed with the close button, error message is being discarded
     $('#closeNewDate').on('click', function() {
             $('#newDayName').removeClass("error");
             $('#newDayNameInfo').text("*");
             $('#newDayNameInfo').removeClass("error");
-            control.log(newDayDate.val());
+
+            $('#newDayDate').removeClass("error");
+            $('#newDayDateInfo').text("*");
+            $('#newDayDateInfo').removeClass("error");
     });
 
 }
